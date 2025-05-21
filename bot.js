10f516be295030a06ef4a5da75d0b9372260067e
boguta-elizaveta <derkachliza@yandex.ru>
2025-05-21 13:44:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply('Добро пожаловать! Чтобы записаться на консультацию, нажмите на кнопку ниже.');
+});
+
+module.exports = bot;
 
