e33c5ae02e78cd2474b85be95a3dab85a3482b9d
boguta-elizaveta <derkachliza@yandex.ru>
2025-05-21 13:43:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const app = express();
+const bot = require('./bot');
+
+const PORT = process.env.PORT || 3000;
+
+// Webhook для Telegram
+app.use(bot.webhookCallback('/bot'));
+
+// Проверка
+app.get('/', (req, res) => res.send('Бот работает!'));
+
+bot.telegram.setWebhook(`${process.env.RENDER_EXTERNAL_URL}/bot`);
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
 
